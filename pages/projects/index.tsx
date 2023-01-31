028f3ac14e038dc3e6b10b4f7dabba451355d923
Anton Metlitsky <ticket1201@gmail.com>
2023-01-31 20:21:08 +0400

    feat: add arrows to glide

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from '../styles/Projects.module.scss'
 import ProjectCard from './ProjectCard/ProjectCard';
 import todo from '../../assets/images/todolist.png'
@@ -12,7 +12,9 @@ import 'node_modules/keen-slider/keen-slider.min.css'
 import {useKeenSlider} from 'keen-slider/react';
 
 const Index = () => {
-    const [sliderRef] = useKeenSlider(
+    const [currentSlide, setCurrentSlide] = useState(0)
+    const [loaded, setLoaded] = useState(false)
+    const [sliderRef, instanceRef] = useKeenSlider(
         {
             slides: {
                 perView: 3,
@@ -28,11 +30,14 @@ const Index = () => {
                         perView: 1,
                     },
                 }
-            }
+            },
+            created() {
+                setLoaded(true)
+            },
+            slideChanged(slider) {
+                setCurrentSlide(slider.track.details.rel)
+            },
         },
-        [
-            // add plugins here
-        ]
     )
 
     return (
@@ -69,9 +74,55 @@ const Index = () => {
                                      projectUrl={'https://ticket1201.github.io/samurai-way/'}
                                      projectDescription={'Online platform to connect people'}/>
                     </div>
+                    {loaded && instanceRef.current && (
+                        <>
+                            <Arrow
+                                left
+                                onClick={(e: any) =>
+                                    e.stopPropagation() || instanceRef.current?.prev()
+                                }
+                                disabled={currentSlide === 0}
+                            />
+
+                            <Arrow
+                                onClick={(e: any) =>
+                                    e.stopPropagation() || instanceRef.current?.next()
+                                }
+                                disabled={
+                                    currentSlide ===
+                                    instanceRef.current.track.details.slides.length - 1
+                                }
+                            />
+                        </>
+                    )}
                 </div>
             </div>
         </Layout>
+    )
+}
+
+function Arrow(props: {
+    disabled: boolean
+    left?: boolean
+    onClick: (e: any) => void
+}) {
+    const disabeld = props.disabled ? ` ${s.arrowDisabled}` : ""
+    return (
+        <svg
+            onClick={props.onClick}
+            className={`${s.arrow} ${
+                props.left ? `${s.arrowLeft}` : `${s.arrowRight}`
+            } ${disabeld}`}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+        >
+            {props.left && (
+                <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
+            )}
+            {!props.left && (
+                <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
+            )}
+        </svg>
     )
 }
 
