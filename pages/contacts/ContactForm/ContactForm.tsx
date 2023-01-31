f2dcbb6bd8d651b0582e33fcef82036aa908e035
Anton Metlitsky <ticket1201@gmail.com>
2023-01-31 20:20:13 +0400

    feat: rework form sending

--- pages/contacts/ContactForm/ContactForm.tsx
+++ pages/contacts/ContactForm/ContactForm.tsx
@@ -1,10 +1,10 @@
-import React, {useRef, useState} from 'react';
-import {ErrorMessage, Field, Form, Formik} from 'formik';
+import React, {useState} from 'react';
+import {ErrorMessage, Field, useFormik} from 'formik';
 import s from '../../styles/Contacts.module.scss'
 import {Fade} from 'react-awesome-reveal';
 import {api} from '../../../api/api';
 
-type MyFormValues = {
+export type MyFormValues = {
     name: string
     email: string
     message: string
@@ -14,52 +14,64 @@ const ContactForm = () => {
     let [isSending, setIsSending] = useState(false)
     let [status, setStatus] = useState('')
     const sendStyle = isSending ? s.formFade : s.hide;
-    const initialValues: MyFormValues = {name: '', email: '', message: ''};
-    const form = useRef();
-    const sendEmail = () => {
-        api.sendEmail(setStatus, setIsSending, form)
+    const sendEmail = async (values: MyFormValues) => {
+        await api.sendEmail(setStatus, setIsSending, formik.values)
+        formik.resetForm()
     };
 
-
+    const formik = useFormik({
+        validate: (values) => {
+            const errors = {} as Partial<MyFormValues>;
+            if (!values.name) {
+                errors.name! = 'Type your name';
+            }
+            if (!values.message) {
+                errors.message = 'Enter your message';
+            } else if (values.message.length < 10) {
+                errors.message = 'Type more that 10 symbols';
+            }
+            if (!values.email) {
+                errors.email = 'Type your email';
+            } else if (
+                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            ) {
+                errors.email = 'Invalid email address';
+            }
+            return errors;
+        },
+        initialValues: {
+            name: '',
+            email: '',
+            message: ''
+        } as MyFormValues,
+        onSubmit: (values: MyFormValues) => {
+            sendEmail(values)
+        }
+    })
     return (
         <>
-            <Formik
-                initialValues={initialValues}
-                validate={values => {
-                    const errors = {} as MyFormValues;
-                    if (!values.name) {
-                        errors.name! = 'Type your name';
-                    }
-                    if (!values.message) {
-                        errors.message = 'Enter your message';
-                    } else if (values.message.length < 10) {
-                        errors.message = 'Type more that 10 symbols';
-                    }
-                    if (!values.email) {
-                        errors.email = 'Type your email';
-                    } else if (
-                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-                    ) {
-                        errors.email = 'Invalid email address';
-                    }
-                    return errors;
-                }}
-                onSubmit={sendEmail}
+            <form className={s.form}
+                  name={'Portfolio Index Form'}
+                  onSubmit={formik.handleSubmit}
             >
-                {({isSubmitting}) => <Form className={s.form}
-                    // @ts-ignore
-                                           ref={form}
-                                           name={'Portfolio Index Form'}>
-                    <Field name="name" placeholder="Name" className={s.input}/>
-                    <ErrorMessage name="name" component="p"/>
-                    <Field name="email" placeholder="Email" className={s.input}/>
-                    <ErrorMessage name="email" component="p"/>
-                    <Field name="message" placeholder="Message text" as="textarea"
-                           className={s.textarea}/>
-                    <ErrorMessage name="message" component="p"/>
-                    <button type="submit" disabled={isSubmitting}>Send message</button>
-                </Form>}
-            </Formik>
+                <input placeholder="Name" className={s.input}
+                       {...formik.getFieldProps('name')}
+                />
+                {formik.errors.name && formik.touched.name ?
+                    <div className="text-rose-600">{formik.errors.name}</div> : null}
+                <input placeholder="Email" className={s.input}
+                       {...formik.getFieldProps('email')}
+                />
+                {formik.errors.email && formik.touched.email ?
+                    <div className="text-rose-600">{formik.errors.email}</div> : null}
+                <textarea placeholder="Message text"
+                          className={s.textarea}
+                          {...formik.getFieldProps('message')}
+                />
+                {formik.errors.message && formik.touched.message ?
+                    <div className="text-rose-600">{formik.errors.message}</div> : null}
+                <button type="submit">Send message</button>
+            </form>
             <Fade className={sendStyle}>
                 <p>{status}</p>
             </Fade>
